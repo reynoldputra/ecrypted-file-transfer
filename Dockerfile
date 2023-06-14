2e41977b5780c9249425cd591b76e8fc9c03812b
ahnafms <ahnafmusyaffa47@gmail.com>
2023-06-14 09:48:38 +0700

    feat: add scp client

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,10 @@ FROM ubuntu:latest
 # Install SSH server
 RUN apt-get update && apt-get install -y openssh-server
 
+# Copy main program
+COPY client-cli /home/client-cli
+COPY server-api /home/server-api
+
 # Generate SSH keys
 RUN mkdir /var/run/sshd
 RUN echo 'root:password' | chpasswd
--- client-cli/script.py
+++ client-cli/script.py
@@ -1,3 +1,6 @@
+from paramiko import SSHClient
+import paramiko
+from scp import SCPClient
 import typer
 from lib.crypto.decrypt import decrypt_file
 from lib.crypto.encrypt import encrypt_file
@@ -21,6 +24,21 @@ def encrypt(file:str, public_key_file:str):
 @app.command()
 def decrypt(file:str, private_key_file:str):
     decrypt_file(file, private_key_file)
+
+@app.command()
+def send():
+    hostname = '172.31.0.4'
+    username = 'root'
+    password = 'password'
+    # local_path = './requirements.txt'
+    # remote_path = '/home/requirements.txt'
+    ssh = SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname=hostname, username=username, password=password)
+
+    scp = SCPClient(ssh.get_transport())
+    scp.put('requirements.txt', recursive=True, remote_path='/home')
+    scp.close()
 
 if __name__ == "__main__":
     app()
