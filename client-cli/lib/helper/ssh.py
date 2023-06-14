c25a36eac8dda5158d68433cb0f9ae31e219f1b5
reynoldputra <reynoldputra1@gmail.com>
2023-06-14 21:29:39 +0700

    feat: add helper scp ssh

---
+++ client-cli/lib/helper/ssh.py
@@ -0,0 +1,12 @@
+def runCommand(ssh, command):
+    # Execute the mkdir command to create a new directory
+    command = ""
+    stdin, stdout, stderr = ssh.exec_command(command)
+
+    # Check the output for any errors
+    error = stderr.read().decode().strip()
+    if error:
+        print(f"Error occurred : {error}")
+    else:
+        print(f"Command success")
+
---
+++ client-cli/lib/helper/transfer.py
@@ -0,0 +1,17 @@
+from scp import SCPClient
+import typer
+from paramiko import SSHClient
+import paramiko
+
+def scp(src, dest):
+    hostname = 'localhost'
+    username = typer.prompt('username')
+    password = typer.prompt('password', hide_input=True)
+
+    ssh = SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname=hostname, username=username, password=password)
+
+    scp = SCPClient(ssh.get_transport())
+    scp.put(src, recursive=True, remote_path=dest)
+    scp.close()
